6007b2791ce0c74de94b57816d2ffcb7a4ab8d8d
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2023-03-25 20:13:00 +0530

    Updates

---
+++ precache-manifest.2deaaca9fbb6943bbfdbb2ac5f9cf93b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0339cf3caa7cd274d31a9e0e12df2453",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e17cbf53da5975c492c",
+    "url": "/static/css/2.58570433.chunk.css"
+  },
+  {
+    "revision": "8936870ef71d445ec3ce",
+    "url": "/static/css/main.3c58cc0b.chunk.css"
+  },
+  {
+    "revision": "9e17cbf53da5975c492c",
+    "url": "/static/js/2.2476181e.chunk.js"
+  },
+  {
+    "revision": "8936870ef71d445ec3ce",
+    "url": "/static/js/main.2e3e96ac.chunk.js"
+  },
+  {
+    "revision": "c9e12d71a4932cc6817f",
+    "url": "/static/js/runtime-main.f180add5.js"
+  },
+  {
+    "revision": "3c164cd8dd969a2afd618464794e3bb0",
+    "url": "/static/media/Dharmik_Patel_Resume.3c164cd8.pdf"
+  },
+  {
+    "revision": "2df3104c113c14e2b36f103087a35431",
+    "url": "/static/media/bankist.2df3104c.png"
+  },
+  {
+    "revision": "fd00452f3d9346b68e859d107edf5168",
+    "url": "/static/media/foad_logo.fd00452f.jpg"
+  },
+  {
+    "revision": "28634f939725644a1b02fb009721a19b",
+    "url": "/static/media/guessNum.28634f93.png"
+  },
+  {
+    "revision": "936368ee3271ac913fbbe414e070a2c8",
+    "url": "/static/media/myBank.936368ee.png"
+  },
+  {
+    "revision": "0bb46bf1b643b8968763f46b8a229025",
+    "url": "/static/media/omnifood.0bb46bf1.png"
+  },
+  {
+    "revision": "7f6ef7c81d9d8527a5ef6a30024942dc",
+    "url": "/static/media/pigGame.7f6ef7c8.png"
+  },
+  {
+    "revision": "18d0ef12ab13f84ffecdb92299a6bca0",
+    "url": "/static/media/programmer1.18d0ef12.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d67895f9e60914915060ff377940e7a5.js"
+  "/precache-manifest.2deaaca9fbb6943bbfdbb2ac5f9cf93b.js"
 );
 
 self.addEventListener('message', (event) => {
