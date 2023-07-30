3130bd8a5ad9ceacdf6211fee9ae1397ee87e7ec
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2023-07-30 13:49:09 +0530

    projects added

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -12,6 +12,9 @@ import omnifood from "../../assets/img/omnifood.jpeg";
 import citylight from "../../assets/img/citylight.png";
 import bankist from "../../assets/img/bankist.png";
 import sociopedia from "../../assets/img/sociopedia.jpeg";
+import movieland from "../../assets/img/movieland.jpeg";
+import imagetotext from "../../assets/img/imagetotext.png";
+import codeeditor from "../../assets/img/codeeditor.png";
 import guessNum from "../../assets/img/guessNum.png";
 
 class Projects extends Component {
@@ -51,16 +54,7 @@ class Projects extends Component {
                     demoLink="https://restaurant-app-3.web.app/"
                   />
                 </Col>
-                <Col md={4} className="project-card">
-                  <ProjectCard
-                    imgPath={omnifood}
-                    isBlog={false}
-                    title="Omnifood"
-                    description="Omnifood is an food subscription website that will make you eat healthy again build with HTML, CSS and Vanilla Javascript. Containing features of real form and good loking UI."
-                    ghLink="https://github.com/Dharmik3/Omnifood-project"
-                    demoLink="https://healthyomnifood.netlify.app/"
-                  />
-                </Col>
+                
 
                 
                 <Col md={4} className="project-card">
@@ -73,7 +67,46 @@ class Projects extends Component {
                     demoLink="https://dharmik3.github.io/sociopedia"
                   />
                 </Col>
-
+                <Col md={4} className="project-card">
+                  <ProjectCard
+                    imgPath={movieland}
+                    isBlog={false}
+                    title="Movieland"
+                    description="A movieland site developed using React and used API which gives all the essential information about the movies. Website will generate all the keyword related results. Users can able to see the trailer video of movie"
+                    ghLink="https://github.com/Dharmik3/movieland"
+                    demoLink="https://dharmik3.github.io/movieland/"
+                  />
+                </Col>
+                <Col md={4} className="project-card">
+                  <ProjectCard
+                    imgPath={omnifood}
+                    isBlog={false}
+                    title="Omnifood"
+                    description="Omnifood is an food subscription website that will make you eat healthy again build with HTML, CSS and Vanilla Javascript. Containing features of real form and good loking UI."
+                    ghLink="https://github.com/Dharmik3/Omnifood-project"
+                    demoLink="https://healthyomnifood.netlify.app/"
+                  />
+                </Col>
+                <Col md={4} className="project-card">
+                  <ProjectCard
+                    imgPath={imagetotext}
+                    isBlog={false}
+                    title="Image to Text Converter"
+                    description="Image to text converter developed using React and tesseract.js library, which provide pure javascript OCR. User can able to copy the text on to clipboard directly."
+                    ghLink="https://github.com/Dharmik3/Image-to-Text-in-React"
+                    demoLink="https://image-to-text-dharmik3.vercel.app/"
+                  />
+                </Col>
+                <Col md={4} className="project-card">
+                  <ProjectCard
+                    imgPath={codeeditor}
+                    isBlog={false}
+                    title="CodersGround"
+                    description="Coders ground is a online code editor, to run HTML,CSS and JS in browser without additional local environment setup.Users can choose their themes, and auto complete the syntax and store the previously written code in localstorage."
+                    ghLink="https://github.com/Dharmik3/codersground"
+                    demoLink="https://codersground.vercel.app/"
+                  />
+                </Col>
                 <Col md={4} className="project-card">
                   <ProjectCard
                     imgPath={bankist}
